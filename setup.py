a9ec4ec6f3d64ab1fe98c01b58f3775be56513da
bgroveben <bgroveben@gmail.com>
2016-07-25 02:55:39 -0700

    Fix typo in setup.py file

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 setup(
     name='django-polls',
     version='0.1',
-    packages=find-packages(),
+    packages=find_packages(),
     include_package_data=True,
     license='MIT License',
     description='A simple Django app to conduct Web-based polls.',
